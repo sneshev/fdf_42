d15171acc7fa261f447312ac10f417e014a6a2bf
John Doe <steffal.n2@gmail.com>
2025-09-12 19:42:11 +0200

    +draw_front_view() | working only needs colors

--- src/draw/orthographic.c
+++ src/draw/orthographic.c
@@ -4,18 +4,41 @@
 #define MIN_Z 2
 #define MAX_Z 3
 
-// void	draw_vertical_lines(t_data data, int grid_data[4])
-// {
-// 	int	x;
-// 	int hpp = data.map->height[PIXEL] / data.map->height[REAL];
-// 	int	wpp = data.map->width[PIXEL] / data.map->width[REAL];
-
-// }
-
-void	draw_horizontal_lines(t_data data, int grid_data[4])
+void	draw_vertical_lines(t_data data, t_map *map, int grid_data[4])
 {
-	(void)grid_data;
-	t_map	*map;
+	float	step_x;
+	float	step_y;
+	int		col;
+	int		j;
+	int		cd[2];
+	int		ncd[2];
+	int		color[2] = {GREEN, GREEN};
+
+	step_x = (float)map->width[PADDED] / (float)(map->width[REAL] - 1);
+	step_y = (float)map->height[PADDED] / (float)(grid_data[MAX_Z] - grid_data[MIN_Z]);
+	col = 0;
+	while (col < map->width[REAL])
+	{
+		j = 0;
+		while (j < map->height[REAL])
+		{
+			cd[X] = map->x[MIN] + col * step_x;
+			cd[Y] = map->y[MAX] - map->points[j][col].value * step_y;
+			j++;
+			ncd[X] = map->x[MIN] + col * step_x;
+			if (j < map->height[REAL])
+			{
+				ncd[Y] = map->y[MAX] - map->points[j][col].value * step_y;
+				draw_coordinate_line(data, cd, ncd, color);
+			}
+		}
+		col++;
+	}
+	mlx_put_image_to_window(data.mlxt.mlx, data.mlxt.win, data.mlxt.img, 0, 0);
+}
+
+void	draw_horizontal_lines(t_data data, t_map *map, int grid_data[4])
+{
 	float	step_x;
 	float	step_y;
 	int		i;
@@ -24,13 +47,11 @@ void	draw_horizontal_lines(t_data data, int grid_data[4])
 	int		ncd[2];
 	int		color[2] = {GREEN, GREEN};
 
-	map = data.map;
 	step_x = (float)map->width[PADDED] / (float)(map->width[REAL] - 1);
 	step_y = (float)map->height[PADDED] / (float)(grid_data[MAX_Z] - grid_data[MIN_Z]);
 	row = 0;
 	while(row < map->height[REAL])
 	{
-		printf("\nrow %d\n", row);
 		i = 0;
 		while (i < map->width[REAL])
 		{
@@ -40,10 +61,7 @@ void	draw_horizontal_lines(t_data data, int grid_data[4])
 			ncd[X] = map->x[MIN] + i * step_x;
 			ncd[Y] = map->y[MAX] - map->points[row][i].value * step_y;
 			if (i < map->width[REAL])
-			{
-				printf("drawing(%d): x {%d->%d}, y {%d->%d}\n", i, cd[X], ncd[X], cd[Y], ncd[Y]);
 				draw_coordinate_line(data, cd, ncd, color);
-			}
 		}
 		row++;
 	}
@@ -59,8 +77,8 @@ void	draw_front_view(t_data data)
 	// ft_printf("x: %d, y: %d, min_z: %d, max_z: %d\n", 
 		// grid_data[X], grid_data[Y], grid_data[MIN_Z], grid_data[MAX_Z]);
 	
-	// draw_vertical_lines(data, grid_data);
-	draw_horizontal_lines(data, grid_data);
+	draw_horizontal_lines(data, data.map, grid_data);
+	draw_vertical_lines(data, data.map, grid_data);
 }
 
 // void	draw_side_view(t_data data)
